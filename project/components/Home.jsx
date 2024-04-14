110cd19b2b519d0670936d7c643aee31e6213456
AhmettYAVUZ <yavuzahmet555@gmail.com>
2024-04-14 22:07:25 +0300

    Anasayfa Düzeni

--- project/components/Home.jsx
+++ project/components/Home.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import "./home.css";
+
 function Home() {
   return (
     <>
-      <div>Home</div>
+      <div className="home">
+        <div className="home-container">
+          <img
+            src="../../Assets/mile1-assets/logo.svg"
+            alt="Teknolojik Yemekler"
+          />
+          <p>KOD ACIKTIRIR</p>
+          <p>PİZZA, DOYURUR</p>
+          <Link className="link" to="/OrderPizza">
+            ACIKTIM
+          </Link>
+        </div>
+      </div>
     </>
   );
 }
--- project/components/OrderPizza.jsx
+++ project/components/OrderPizza.jsx
@@ -1,7 +1,37 @@
+import { FormGroup, Input, Label } from "reactstrap";
+import "./orderpizza.css";
+
 export default function OrderPizza() {
   return (
     <>
-      <div>Order Pizza</div>
+      <div>
+        <header className="header">
+          <img
+            src="../../Assets/mile1-assets/logo.svg"
+            alt="Teknolojik yemekler"
+          />
+          <nav>
+            <a href="/">Anasayfa</a>-<a href="/">Seçenekler</a>-
+            <a href="/">Sipariş Oluştur</a>
+          </nav>
+        </header>
+        <div>
+          <p>Position Absolute Acı Pizza</p>
+          <p>84.50</p>
+          <p>4.9</p>
+          <p>(200)</p>
+          <FormGroup tag="fieldset">
+            <legend>Radio Buttons</legend>
+            <FormGroup check>
+              <Input name="radio1" type="radio" />{" "}
+              <Label check>
+                Option one is this and that—be sure to include why it‘s great
+              </Label>
+            </FormGroup>
+          </FormGroup>
+          <p></p>
+        </div>
+      </div>
     </>
   );
 }
