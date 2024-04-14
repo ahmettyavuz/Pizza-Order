eab291b8c102962991c3e8bd3f01e35290bb9538
AhmettYAVUZ <yavuzahmet555@gmail.com>
2024-04-14 23:47:21 +0300

    OrderPizza  Sayfa Düzeni

--- project/components/OrderPizza.jsx
+++ project/components/OrderPizza.jsx
@@ -15,21 +15,111 @@ export default function OrderPizza() {
             <a href="/">Sipariş Oluştur</a>
           </nav>
         </header>
-        <div>
+        <div className="main">
           <p>Position Absolute Acı Pizza</p>
           <p>84.50</p>
           <p>4.9</p>
           <p>(200)</p>
+          <p>
+            Frontent Dev olarak hala position:absolute kullanıyorsan bu çok acı
+            pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli
+            diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun
+            ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle
+            yuvarlak, düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan
+            kökenli lezzetli bir yemektir. . Küçük bir pizzaya bazen pizzetta
+            denir.
+          </p>
           <FormGroup tag="fieldset">
-            <legend>Radio Buttons</legend>
+            <legend>Boyut Seç</legend>
             <FormGroup check>
-              <Input name="radio1" type="radio" />{" "}
-              <Label check>
-                Option one is this and that—be sure to include why it‘s great
-              </Label>
+              <Input name="Küçük" type="radio" /> <Label check>Küçük</Label>
+            </FormGroup>
+            <FormGroup check>
+              <Input name="Orta" type="radio" /> <Label check>Orta</Label>
+            </FormGroup>
+            <FormGroup check>
+              <Input name="Büyük" type="radio" /> <Label check>Büyük</Label>
             </FormGroup>
           </FormGroup>
-          <p></p>
+          <FormGroup>
+            <Label for="exampleSelect">Hamur Seç</Label>
+            <Input id="exampleSelect" name="select" type="select">
+              <option>Hamur Kalınlığı</option>
+              <option>ince</option>
+              <option>kalın</option>
+            </Input>
+          </FormGroup>
+          <legend>Ek Malzemeler</legend>
+          <p>En Fazla 10 malzeme seçebilirsiniz. 5</p>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Pepperoni</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Domates</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Biber</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Sosis</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Mısır</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Sucuk</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Kanada Jambonu</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Ananas</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Tavuk Izgara</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Tavuk Izgara</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Tavuk Izgara</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Tavuk Izgara</Label>
+          </FormGroup>
+          <FormGroup check inline>
+            <Input type="checkbox" />
+            <Label check>Tavuk Izgara</Label>
+          </FormGroup>
+          <FormGroup>
+            <Label for="exampleText">Sipariş Notu</Label>
+            <Input id="exampleText" name="text" type="textarea" />
+          </FormGroup>
+          <p>----------------------------------</p>
+          <div>
+            <button>-</button>
+            <p>sayı</p>
+            <button>+</button>
+          </div>
+          <div>
+            <p>Sipariş Toplamı</p>
+            <p>Seçimler</p>
+            <p>seçim toplam</p>
+            <p>toplam</p>
+            <p>toplam tl</p>
+          </div>
         </div>
       </div>
     </>
