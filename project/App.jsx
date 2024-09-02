b1bdda4220086dd8436a8f46919816af0441fe3f
AhmettYAVUZ <yavuzahmet555@gmail.com>
2024-09-02 13:26:08 +0300

    deneme

---
+++ project/App.jsx
@@ -0,0 +1,34 @@
+import { Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import OrderPizza from "./components/OrderPizza";
+import Success from "./components/Success";
+import { useState } from "react";
+
+function App() {
+  const [siparisData, setSiparisData] = useState({});
+
+  const data = (data) => {
+    setSiparisData(data);
+  };
+
+  return (
+    <>
+      <Switch>
+
+
+        <Route exact path="/home">
+
+          <Home />
+        </Route>
+        <Route exact path="/OrderPizza">
+          <OrderPizza data={data} />
+        </Route>
+        <Route exact path="/Success">
+          <Success siparisData={siparisData} />
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
---
+++ project/components/Success.jsx
@@ -0,0 +1,56 @@
+import { useHistory } from "react-router-dom";
+import "./success.css";
+function Success(props) {
+  const { siparisData } = props;
+
+  return (
+    <>
+      <div className="success">
+        <div className="success-container">
+          <img
+            src="../../Assets/mile1-assets/logo.svg"
+            alt="Teknolojik Yemekler"
+          />
+
+          <div className="success-content">
+            <p>lezzetin yolda</p>
+            <p>SİPARİŞİNİZ ALINDI!</p>
+            <hr />
+            <p>Position Absolute Acı Pizza</p>
+          </div>
+
+          <div className="success-data">
+            <div>
+              <p>Boyut:</p>
+              <p>{siparisData.boyut}</p>
+            </div>
+            <div>
+              <p>Hamur:</p>
+              <p>{siparisData.hamur}</p>
+            </div>
+            <div>
+              <p>Ek Malzemeler:</p>
+              <p>{siparisData.malzemeler.join(", ")}</p>
+            </div>
+          </div>
+
+          <div className="success-sipariscard">
+            <div>
+              <p>Sipariş Toplamı</p>
+            </div>
+            <div>
+              <p>Seçimler</p>
+              <p>{siparisData.malzemeler.length * 5 * siparisData.adet}₺</p>
+            </div>
+            <div>
+              <p>Toplam</p>
+              <p>{siparisData.total()}₺</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Success;
